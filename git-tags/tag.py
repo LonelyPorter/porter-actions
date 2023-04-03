86343a5ad4f769b2e073599269afba21591b80ae
Just_A_Lonely_Porter <76800139+LonelyPorter@users.noreply.github.com>
2023-04-03 02:10:24 +0000

    feat: update script to parse project.toml

--- git-tags/tag.py
+++ git-tags/tag.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
-print("Hello World!")
+import tomllib as tb
+
+class Project:
+    def __init__(self, name:str, version: str):
+        self.name = name
+
+        self.major = version.split('.')[0]
+        self.minor = version.split('.')[1]
+        self.patch = version.split('.')[2]
+
+if __name__ == "__main__":
+    with open('project.toml', mode='rb') as fp:
+        project = tb.load(fp)
+        
+        name = project['Project']['name']
+        version = project['Project']['version']
+
+        p = Project(name, version)
